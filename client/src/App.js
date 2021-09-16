1a71f3dfbe4c3f748d2c6d499929c437e48495ba
Secret8890 <gml8890@naver.com>
2021-09-16 17:18:25 +0900

    Add Customer API LOADING

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ import TableBody from "@material-ui/core/TableBody";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
 import { withStyles } from "@material-ui/core/styles";
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 
 const styles = (theme) => ({
   root: {
@@ -18,21 +20,47 @@ const styles = (theme) => ({
   table: {
     minWidth: 1080,
   },
+  progress : {
+    margin : theme.spacing(2),
+  }
 });
+/*
+
+  1) Constructor()
+
+  2) ComponentWillMount()
+
+  3) render()
+
+  4) componentDidMount()
+
+*/
+/*
+
+  Change - props or state => shouldComponentUpdate()
+
+*/
 
 class App extends React.Component {
   state = {
     customers : "" ,
+    completed : 0
   }
   componentDidMount() {
+    this.timer = setInterval(this.progress, 30);
     this.callApi()
     .then(res => this.setState({customers : res}))
     .catch(err=>console.log(err));
+    
   }
   callApi = async () => {
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
+  }
+  progress = () => {
+    const {completed} = this.state;
+    this.setState({completed : (completed >= 100 ? 0 : completed + 1)});
   }
   render() {
     const {classes} = this.props;
@@ -62,7 +90,13 @@ class App extends React.Component {
                   job={c.job}
                 />
               )
-            }):""}
+            }):
+              <TableRow>
+                <TableCell colSpan = '6' align="center">
+                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+                </TableCell>
+              </TableRow>
+              }
           </TableBody>
         </Table>
       </Paper>
