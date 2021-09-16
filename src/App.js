ac19e474b0dde2da8daa3df6c91a3d3fcb7e5fab
Secret8890 <gml8890@naver.com>
2021-09-16 15:30:22 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,57 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import Customer from "./Customer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="gray-background">
-          <img src={logo} lat = "logo"/>
-          <h2>Let's develop management system !</h2>
-        </div>
-      </header>
-    </div>
-  );
+const customer = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/1",
+    name: "홍길동",
+    birthday: "19950109",
+    gender: "남자",
+    job: "대학생",
+  },
+  
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/2",
+    name: "강감찬",
+    birthday: "19950109",
+    gender: "남자",
+    job: "귀주대첩",
+  },
+  
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/3",
+    name: "광개토대왕",
+    birthday: "19950109",
+    gender: "남자",
+    job: "고구려전성시대의 왕",
+  },
+  
+];
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        {
+          customer.map(c => {
+            return (
+              <Customer 
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name = {c.name}
+              birthday = {c.birthday}
+              gender = {c.gender}
+              job = {c.job}/>
+
+            )
+          })
+        }
+      </div>
+    );
+  }
 }
-
 export default App;
